7f0d0c6135942c7aa44625868059a583f59637ed
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-09-04 08:14:50 -0700

    Update src/Components/Button.jsx and Provide onClose prop to Modal in Homepage.jsx.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,14 @@
 import { Plus } from "lucide-react";
 
-const Button = () => {
+const Button = ({ onClick, type }) => {
     return (
-        <button className="bg-white rounded-full text-black cursor-pointer">
+        <button className="bg-white rounded-full text-black cursor-pointer"
+        onClick={() => {
+            if (onClick) {
+                onClick();
+                type = "task";
+            }}}
+        >
             <Plus strokeWidth={2} />
         </button>
     );
