ec6fefa3e275c30ad9732d921573432a2c3db3cf
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-09-04 04:09:16 -0700

    Create and Add TaskList.jsx component to display list of tasks on homepage. Use a default data array to map through and display each task item.

---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,33 @@
+import TaskItem from "./Taskitem";
+
+const TaskList = () => {
+  // Dummy task data
+  const tasks = [
+    {
+      title: "Design Landing Page",
+      dueDate: "Tomorrow",
+      tags: ["UI/UX", "High Priority"],
+    },
+    {
+      title: "Build API Endpoints",
+      dueDate: "Friday",
+      tags: ["Back-End", "Medium Priority"],
+    },
+  ];
+  return (
+    <>
+      <div className="space-y-4">
+        {tasks.map((task, index) => (
+          <TaskItem
+            key={index}
+            title={task.title}
+            dueDate={task.dueDate}
+            tags={task.tags}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default TaskList;
