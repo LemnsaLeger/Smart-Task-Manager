a4547450f3abf6ab8182802e1b719e8ab734edf2
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-09-04 05:24:34 -0700

    update TaskItem component, add a blur background.

--- src/Components/Taskitem.jsx
+++ src/Components/Taskitem.jsx
@@ -2,19 +2,22 @@
 
 const TaskItem = ({ title, dueDate, tags }) => {
     return (
-        <article className="glass-card p-4 flex items-center justify-between backdrop-blur-md">
-            <div>
-                <h4 className="font-semibold text-lg"> {title} </h4>
-                <p className="text-gray-400 text-sm">Due: {dueDate}</p>
-            </div>
-            <p className="flex items-center space-x-2">
-                {tags.map((tag, index) => (
-                    <span key={index} className={`bg-blue-600 text-white text-xs font-semibold`}>
-                        {tag}
-                    </span>
-                ))}
-            </p>
-        </article>
+      <article className="glass-card p-4 flex items-center backdrop-blur bg-white/30 rounded shadow justify-between">
+        <div className="relative">
+          <h4 className="font-semibold text-lg"> {title} </h4>
+          <p className="text-gray-400 text-sm ">Due: {dueDate}</p>
+        </div>
+        <p className="flex items-center space-x-2 relative">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className={`bg-black text-white text-[0.5em] font-semibold p-1 rounded-xl text-center`}
+            >
+              {tag}
+            </span>
+          ))}
+        </p>
+      </article>
     );
 }
 
