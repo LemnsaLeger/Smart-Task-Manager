a0f9b65f0366a4864bc908340f54e7e415f4613c
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-09-21 11:50:31 -0700

    Update TaskList.jsx and HandleEdit Modal, Handle Create Modal, Pass Delete and Update props to Taskitem.

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,24 +1,62 @@
 import TaskItem from "./Taskitem";
+import Button from "./Button";
+import { ChevronLeftIcon, Plus } from "lucide-react";
+const TaskList = ({
+  tasks = [],
+  openTask,
+  setEditingTask,
+  openModal,
+  deleteTask,
+  handleBackHome,
+}) => {
 
-const TaskList = ({openTask, tasks = []}) => {
+  const handleOpenEditModal = () => {
+    // this function makes sure we are opening the correct type
+    openModal("task");
+  }
 
-
-
+  const handleOpenCreateModal = () => {
+    setEditingTask(null);
+    openModal("task");
+  }
   return (
     <>
-      <div className="space-y-4 mt-3  rounded-xl">
-        {tasks.map((task, index) => (
-          <TaskItem
-            key={index}
-            title={task.title}
-            dueDate={task.dueDate}
-            tags={task.tags}
-            onOpen={() => openTask(task)}
-          />
-        ))}
+      <div className="space-y-4 mt-3  rounded-xl p-2 relative">
+        {tasks.length === 0 ? (
+          <>
+            <p className="text-gray-700  fixed top-[20vh] left-20">
+              No tasks created yet!
+            </p>
+              <button
+                onClick={handleOpenCreateModal}
+                className="absolute right-0 top-[-.8rem] mt-4 bg-gray-800 text-white px-4 py-2 rounded-full flex items-center gap-2 hover:bg-gray-700 transition-colors"
+              >
+                <Plus size={16} />
+                Create New Task
+              </button>
+          </>
+        ) : (
+          tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onOpen={() => openTask(task)}
+              setEditingTask={setEditingTask}
+              openModal={handleOpenEditModal}
+              deleteTask={deleteTask}
+            />
+          ))
+        )}
+        <button
+          className="fixed top-[10vh] left-2 border p-2 rounded-2xl flex"
+          onClick={() => handleBackHome()}
+        >
+          <ChevronLeftIcon />
+          Back
+        </button>
       </div>
     </>
   );
-}
+};
 
 export default TaskList;
