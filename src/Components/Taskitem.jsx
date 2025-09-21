cbc1de1f2d978031e919243eacdbb5acf26e1917
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-09-21 11:52:53 -0700

    Update Taskitem.jsx and Handle setEditingTask, openModal, deleteTask.

--- src/Components/Taskitem.jsx
+++ src/Components/Taskitem.jsx
@@ -1,13 +1,27 @@
+import { PenLine, Trash2 } from "lucide-react";
+
+import Modal from "./Modal";
 
 
-const TaskItem = ({ title, dueDate, tags }) => {
-    return (
-      <article className="glass-card p-4 flex items-center backdrop-blur bg-white/30 rounded shadow justify-between">
-        <div className="relative">
-          <h4 className="font-semibold text-lg"> {title} </h4>
+const TaskItem = ({ task, onOpen, setEditingTask, openModal, deleteTask }) => {
+  const { taskTitle, dueDate, tags = [] } = task;
+
+  const handleEditClick = (e) => {
+    e.stopPropagation();
+    setEditingTask(task);
+    openModal();
+  }
+  return (
+    <>
+      <article
+        key={taskTitle}
+        className="glass-card p-4 flex items-center backdrop-blur bg-white/30 rounded shadow justify-between"
+      >
+        <div className="relative" onClick={() => onOpen(task)}>
+          <h4 className="font-semibold text-lg text-black"> {taskTitle} </h4>
           <p className="text-gray-400 text-sm ">Due: {dueDate}</p>
         </div>
-        <p className="flex items-center space-x-2 relative">
+        <p className="flex items-center space-x-2 relative -bottom-4">
           {tags.map((tag, index) => (
             <span
               key={index}
@@ -17,8 +31,24 @@ const TaskItem = ({ title, dueDate, tags }) => {
             </span>
           ))}
         </p>
+        <p className="absolute stroke-1 flex gap-2 top-0 right-3">
+          <PenLine
+            size={20}
+            className="cursor-pointer hover:stroke-blue-500"
+            onClick={handleEditClick}
+          />
+          <Trash2
+            size={20}
+            className="cursor-pointer hover:stroke-red-500"
+            onClick={(e) => {
+              e.stopPropagation();
+              deleteTask(task.id);
+            }}
+          />
+        </p>
       </article>
-    );
-}
+    </>
+  );
+};
 
 export default TaskItem;
