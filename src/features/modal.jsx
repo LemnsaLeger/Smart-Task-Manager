2c1126b3845e31b97c1e5873d4b0628ef90ab23b
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-09-28 09:31:46 -0700

    Update modal.jsx And Add select interval logic. Update prop names to be clear and Add any remaining Prop.

--- src/features/modal.jsx
+++ src/features/modal.jsx
@@ -1,9 +1,9 @@
-
-const Modal = ({modal, intervals, handleModal, title}) => {
+import { X } from "lucide-react";
+const Modal = ({ show, intervals, handleClose, title, onSelectInterval }) => {
    return (
      <div
        className={`modal absolute inset-0 w-full h-full bg-white/50 backdrop-blur-md ${
-         modal ? "block" : "hidden"
+         show ? "block" : "hidden"
        }`}
      >
        <div className="modal-content place-self-center w-60 h-60 mt-60  bg-gray-100 rounded relative">
@@ -11,14 +11,15 @@ const Modal = ({modal, intervals, handleModal, title}) => {
            {title}
          </h3>
          <button
-           onClick={handleModal}
+           onClick={handleClose}
            className="close-modal absolute right-2 top-3 bg-gray-200 rounded-2xl p-1"
          >
            <X />
          </button>
          <ul className={`intervals grid p-2  grid-row-5 gap-4`}>
            {intervals.map((int, index) => (
-             <li key={index}>
+             <li key={index}
+                 onClick={() => onSelectInterval(int)}>
                {int}mins {`${index === 0 ? "(Default)" : ""}`}
              </li>
            ))}
